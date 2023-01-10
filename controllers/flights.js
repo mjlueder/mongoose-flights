7e778f1f22a20ae2db7342223979c14d53bf5124
Mary Lueder <mlueder@gmail.com>
2023-01-09 21:16:38 -0500

    add new and index functionality, departs date

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,17 +1,36 @@
 import { Flight } from '../models/flight.js'
 
 function index(req, res){
-  res.render('flights/index', {
-    title: 'All Flights'
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      title: 'All Flights',
+      flights: flights
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
   })
 }
 
 function newFlight(req, res){
-
+  res.render('flights/new', {
+    title: 'Add Flight'
+  })
 }
 
 function create(req, res){
-
+  console.log(req.body)
+  Flight.create(req.body)
+  .then(flight => {
+    console.log(flight.departs)
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
 }
 
 function edit(req, res){
--- models/flight.js
+++ models/flight.js
@@ -4,9 +4,14 @@ const Schema = mongoose.Schema
 
 const flightSchema = new Schema ({
   airline: String,
-  airport: String,
+  airport: {type: String, default: 'DEN'},
   flightNo: Number,
-  departs: Date,
+  departs: {
+    type: Date,
+    default: function(){
+      return new Date(Date.now() + 365*24*60*60000)
+    }
+  }
 }, {
   timestamps: true
 })
