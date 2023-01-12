134b538cc28f83870ff7d29374b7812e10d48543
Mary Lueder <mlueder@gmail.com>
2023-01-12 11:00:11 -0500

    fix add meal to flight functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -22,7 +22,7 @@ function newFlight(req, res){
 }
 
 function create(req, res){
-  console.log(req.body)
+  // console.log(req.body)
   Flight.create(req.body)
   .then(flight => {
     res.redirect('/flights')
@@ -37,10 +37,19 @@ function show(req, res){
   Flight.findById(req.params.id)
   .populate('meals')
   .then(flight => {
-    Meal.find({ _id: {$nin: flight.meals})
-    res.render('flights/show', {
-      flight,
-      title: 'Flight Details'
+    // console.log('flight', flight);
+    Meal.find({ _id: {$nin: flight.meals}})
+    .then(meals => {
+      // console.log('MEALS!', meals);
+      res.render('flights/show', {
+        flight,
+        title: 'Flight Details', 
+        meals
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
     })
   })
   .catch(err => {
@@ -107,6 +116,27 @@ function createTicket(req, res){
   })
 }
 
+function addMeal(req, res){
+  // console.log('req.body!!', req.body);
+  // console.log('req.params.id!!!', req.params.id);
+  Flight.findById(req.params.id)
+  .then(flight => {
+    flight.meals.push(req.body.mealId)
+    flight.save()
+    .then(() => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
 export {
   index,
   newFlight as new,
@@ -116,5 +146,5 @@ export {
   update,
   deleteFlight as delete,
   createTicket,
-
+  addMeal,
 }
